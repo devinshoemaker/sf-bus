34f98f295a78c6515c449bb834bf976c56b933e6
Devin Shoemaker <devinshoe@gmail.com>
2020-07-31 06:13:36 -0500

    modify home page url

--- apps/sf-bus/src/app/app.tsx
+++ apps/sf-bus/src/app/app.tsx
@@ -30,8 +30,7 @@ export const App = () => {
     <IonApp>
       <IonReactRouter>
         <IonRouterOutlet>
-          <Route path="/home" component={Home} exact={true} />
-          <Route exact path="/" render={() => <Redirect to="/home" />} />
+          <Route exact path="/" component={Home} />
         </IonRouterOutlet>
       </IonReactRouter>
     </IonApp>
